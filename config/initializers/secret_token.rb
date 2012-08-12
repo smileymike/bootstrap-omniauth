0250616f635588ffaccc6979876e4c97f696ff41
Michael Cunnane <smiley.mikey@gmail.com>
2012-08-12 10:46:51 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapOmniauth::Application.config.secret_token = '2d66532534244e1457f723f715d1888a833ce958517c5b954030075a6fccad21031c5cb3db8a95781984af586067e66288e3b7fb42025e0e01d053c29652361b'
